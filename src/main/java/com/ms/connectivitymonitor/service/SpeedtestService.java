4c6b7af9b642c9df2b3a023a6cb9fea665343e10
Martin Schijf <mschijf@bol.com>
2021-10-31 15:20:42 +0100

    1.4 Adding prometheus measurements - speedtest download and upload

--- src/main/java/com/ms/connectivitymonitor/service/SpeedtestService.java
+++ src/main/java/com/ms/connectivitymonitor/service/SpeedtestService.java
@@ -3,6 +3,7 @@ package com.ms.connectivitymonitor.service;
 import com.ms.connectivitymonitor.commandline.ookla.OoklaSpeedTestExecutor;
 import com.ms.connectivitymonitor.entity.SpeedtestData;
 import com.ms.connectivitymonitor.repository.SpeedtestRepository;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,25 +14,36 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class SpeedtestService {
 
     private static final Logger log = LoggerFactory.getLogger(SpeedtestService.class);
-    SpeedtestRepository repository;
-    OoklaSpeedTestExecutor ooklaSpeedTestExecutor;
+    private SpeedtestRepository repository;
+    private OoklaSpeedTestExecutor ooklaSpeedTestExecutor;
+    private AtomicInteger gaugeDownloadSpeed;
+    private AtomicInteger gaugeUploadSpeed;
+
 
     @Autowired
-    public SpeedtestService(SpeedtestRepository repository, OoklaSpeedTestExecutor ooklaSpeedTestExecutor) {
+    public SpeedtestService(SpeedtestRepository repository, OoklaSpeedTestExecutor ooklaSpeedTestExecutor, MeterRegistry registry) {
         this.repository = repository;
         this.ooklaSpeedTestExecutor = ooklaSpeedTestExecutor;
+        gaugeDownloadSpeed = registry.gauge("speedtest.gauge.downloadSpeed", new AtomicInteger(0));
+        gaugeUploadSpeed = registry.gauge("speedtest.gauge.uploadSpeed", new AtomicInteger(0));
     }
 
     public Optional<SpeedtestData> doSpeedTest() {
-        Optional<SpeedtestData> speedTestData = ooklaSpeedTestExecutor.execute();
-        if (speedTestData.isEmpty())
-            return speedTestData;
 
+        Optional<SpeedtestData> speedTestData = ooklaSpeedTestExecutor.execute();
+        if (speedTestData.isEmpty()) {
+            gaugeDownloadSpeed.set(0);
+            gaugeUploadSpeed.set(0);
+            return speedTestData;
+        }
+        gaugeDownloadSpeed.set(speedTestData.get().getDownloadSpeedBytes());
+        gaugeUploadSpeed.set(speedTestData.get().getUploadSpeedBytes());
         SpeedtestData savedData = repository.save(speedTestData.get());
         return Optional.of(savedData);
     }
