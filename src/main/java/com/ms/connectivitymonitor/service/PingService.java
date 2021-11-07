790ffcb9e724a1072536477a396a82747db7fc60
Martin Schijf <mschijf@bol.com>
2021-11-07 19:54:52 +0100

    2.0.2 timers and distributin added

--- src/main/java/com/ms/connectivitymonitor/service/PingService.java
+++ src/main/java/com/ms/connectivitymonitor/service/PingService.java
@@ -5,24 +5,22 @@ import com.ms.connectivitymonitor.entity.PingData;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PingService {
 
     PingExecutor pingExecutor;
-    private AtomicInteger gaugePingMaxMs;
-    private AtomicInteger gaugePingMinMs;
-    private AtomicInteger gaugePingAvgMs;
     private Counter counterPingPackageTransmitted;
     private Counter counterPingPackageReceived;
-    private AtomicInteger gaugePingPackagesMissed;
+    private Timer pingTimer;
 
     @Autowired
     public PingService(PingExecutor pingExecutor, MeterRegistry registry) {
@@ -31,33 +29,25 @@ public class PingService {
     }
 
     private void initMetrics(MeterRegistry registry) {
-        gaugePingMaxMs = registry.gauge("pingtime", Collections.singletonList(new ImmutableTag("level", "max")), new AtomicInteger(0));
-        gaugePingMinMs = registry.gauge("pingtime", Collections.singletonList(new ImmutableTag("level", "min")), new AtomicInteger(0));
-        gaugePingAvgMs = registry.gauge("pingtime", Collections.singletonList(new ImmutableTag("level", "avg")), new AtomicInteger(0));
         counterPingPackageTransmitted = registry.counter("pingpackage", Collections.singletonList(new ImmutableTag("sendtype", "transmitted")));
         counterPingPackageReceived = registry.counter("pingpackage", Collections.singletonList(new ImmutableTag("sendtype", "received")));
-        gaugePingPackagesMissed = registry.gauge("pingpackages_missed", new AtomicInteger(0));
+        pingTimer = Timer.builder("pingtime").register(registry);
     }
 
     public Optional<PingData> doPing() {
-        Optional<PingData> pingData = pingExecutor.execute("kpn.nl", 50, 55);
+        Optional<PingData> pingData = pingExecutor.execute("kpn.nl", 1, 2);
         setMetrics(pingData);
         return pingData;
     }
 
     private void setMetrics(Optional<PingData> pingData) {
-        gaugePingAvgMs.set(pingData.map(PingData::getAvgTimeMillis).orElse(0));
-        gaugePingMinMs.set(pingData.map(PingData::getMinTimeMillis).orElse(0));
-        gaugePingMaxMs.set(pingData.map(PingData::getMaxTimeMillis).orElse(0));
-        int packagesTransmitted = pingData.map(PingData::getPacketsTransmitted).orElse(0);
-        int packagesReceived = pingData.map(PingData::getPacketsReceived).orElse(0);
-
-        counterPingPackageTransmitted.increment(packagesTransmitted);
-        counterPingPackageReceived.increment(packagesReceived);
-        gaugePingPackagesMissed.set(packagesTransmitted - packagesReceived);
+        counterPingPackageTransmitted.increment(pingData.map(PingData::getPacketsTransmitted).orElse(0));
+        counterPingPackageReceived.increment(pingData.map(PingData::getPacketsReceived).orElse(0));
+        pingTimer.record(pingData.map(PingData::getMaxTimeMillis).orElse(0), TimeUnit.MILLISECONDS);
     }
 
-    @Scheduled(cron = "${schedule.runping.cron:-}")
+//    @Scheduled(cron = "${schedule.runping.cron:-}")
+    @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
         doPing();
     }
--- src/main/java/com/ms/connectivitymonitor/service/SpeedtestService.java
+++ src/main/java/com/ms/connectivitymonitor/service/SpeedtestService.java
@@ -2,6 +2,7 @@ package com.ms.connectivitymonitor.service;
 
 import com.ms.connectivitymonitor.commandline.ookla.OoklaSpeedTestExecutor;
 import com.ms.connectivitymonitor.entity.SpeedtestData;
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,8 @@ public class SpeedtestService {
     private final OoklaSpeedTestExecutor ooklaSpeedTestExecutor;
     private AtomicInteger gaugeDownloadSpeed;
     private AtomicInteger gaugeUploadSpeed;
+    DistributionSummary distibutionSummaryUpload;
+    DistributionSummary distibutionSummaryDownload;
 
     @Autowired
     public SpeedtestService(OoklaSpeedTestExecutor ooklaSpeedTestExecutor, MeterRegistry registry) {
@@ -32,6 +35,16 @@ public class SpeedtestService {
     private void initMetrics(MeterRegistry registry) {
         gaugeUploadSpeed = registry.gauge("speedtest_speed_upload", new AtomicInteger(0));
         gaugeDownloadSpeed = registry.gauge("speedtest_speed_download", new AtomicInteger(0));
+        distibutionSummaryUpload = DistributionSummary
+                .builder("internetspeed")
+                .baseUnit("megabits/second")
+                .tags("direction", "upload")
+                .register(registry);
+        distibutionSummaryDownload = DistributionSummary
+                .builder("internetspeed")
+                .baseUnit("megabits/second")
+                .tags("direction", "download")
+                .register(registry);
     }
 
     public Optional<SpeedtestData> doSpeedTest() {
@@ -52,6 +65,12 @@ public class SpeedtestService {
     private void setMetrics(Optional<SpeedtestData> speedTestData) {
         gaugeDownloadSpeed.set(speedTestData.map(SpeedtestData::getDownloadSpeedBytes).orElse(0));
         gaugeUploadSpeed.set(speedTestData.map(SpeedtestData::getUploadSpeedBytes).orElse(0));
+        distibutionSummaryUpload.record(bytesToMBits(speedTestData.map(SpeedtestData::getUploadSpeedBytes).orElse(0)));
+        distibutionSummaryDownload.record(bytesToMBits(speedTestData.map(SpeedtestData::getDownloadSpeedBytes).orElse(0)));
+    }
+
+    private int bytesToMBits(int nBytes) {
+        return nBytes * 8 / 1000000;
     }
 
     @Scheduled(cron = "${schedule.runspeedtest.cron:-}")
