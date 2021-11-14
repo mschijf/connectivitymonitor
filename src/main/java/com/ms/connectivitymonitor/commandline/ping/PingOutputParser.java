828f4f39bc923f49898389b26a87aca3845a344d
Martin Schijf <mschijf@bol.com>
2021-11-14 17:11:51 +0100

    2.0.4 null pointer bug fix, extra log info

--- src/main/java/com/ms/connectivitymonitor/commandline/ping/PingOutputParser.java
+++ src/main/java/com/ms/connectivitymonitor/commandline/ping/PingOutputParser.java
@@ -1,6 +1,8 @@
 package com.ms.connectivitymonitor.commandline.ping;
 
 import com.ms.connectivitymonitor.entity.PingData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -11,29 +13,28 @@ import java.util.regex.Pattern;
 @Component
 public class PingOutputParser {
 
+    private static final Logger log = LoggerFactory.getLogger(PingOutputParser.class);
+
     public Optional<PingData> parsePingOutput(LocalDateTime startTime, String host, String pingOutput) {
         Pattern patternSummary = Pattern.compile("r.+ min/avg/max/.+dev = (.*?)/(.*?)/(.*?)/(.*?) ms\\n");
         Matcher matcherSummary = patternSummary.matcher(pingOutput);
         Pattern patternPackets = Pattern.compile("(.*?) packets transmitted, (.*?)[ packets]* received");
         Matcher matcherPackets = patternPackets.matcher(pingOutput);
 
-        Integer min  = null;
-        Integer avg  = null;
-        Integer max  = null;
-        Integer transmitted = null;
-        Integer received = null;
         try {
-            if (matcherSummary.find() && matcherSummary.groupCount() == 4) {
-                min = parseToInteger(matcherSummary.group(1).trim());
-                avg = parseToInteger(matcherSummary.group(2).trim());
-                max = parseToInteger(matcherSummary.group(3).trim());
+            if (matcherSummary.find() && matcherSummary.groupCount() == 4 && matcherPackets.find() && matcherPackets.groupCount() == 2) {
+                int min = parseToInteger(matcherSummary.group(1).trim());
+                int avg = parseToInteger(matcherSummary.group(2).trim());
+                int max = parseToInteger(matcherSummary.group(3).trim());
+                int transmitted = parseToInteger(matcherPackets.group(1).trim());
+                int received = parseToInteger(matcherPackets.group(2).trim());
+                return Optional.of(new PingData(startTime, transmitted, received, min, avg, max, host));
+            } else {
+                log.error("Error while parsing {}", pingOutput);
+                return Optional.empty();
             }
-            if (matcherPackets.find() && matcherPackets.groupCount() == 2) {
-                transmitted = parseToInteger(matcherPackets.group(1).trim());
-                received = parseToInteger(matcherPackets.group(2).trim());
-            }
-            return Optional.of(new PingData(startTime, transmitted, received, min, avg, max, host));
         } catch (NumberFormatException e) {
+            log.error("Error while formatting to integer {}", pingOutput, e);
             return Optional.empty();
         }
     }
